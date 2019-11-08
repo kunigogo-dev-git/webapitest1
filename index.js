8cadabb07a0e5c4aa140385857cce6df4bd1bc96
kunigogo-dev-git <44841471+kunigogo-dev-git@users.noreply.github.com>
2019-11-08 09:29:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  let n = new Date()
+  response.send('Hello World! @ ' + n.toISOString())
 })
 
 app.get('/api', function(request, response) {
